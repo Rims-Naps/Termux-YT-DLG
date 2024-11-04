57ac6deb077a073b8c65dab99acc116b8df91e6d
Snow-RSPS <108329399+Rims-Naps@users.noreply.github.com>
2024-11-04 14:51:39 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Termux-YTD Installation Script
 
-STORAGE_PATH="/data/data/com.termux/files/home/storage/shared/"
+STORAGE_PATH="/data/data/com.termux/files/home/storage/shared"
 
 # Update and upgrade Termux packages
 echo "Updating Termux packages..."
@@ -11,42 +11,32 @@ apt-get update && apt-get upgrade -y
 # Request storage permission for Termux
 echo "Requesting storage access..."
 termux-setup-storage
-sleep 4
+sleep 2
 
-# Install Python and yt-dlp for downloading media
-echo "Installing Python and yt-dlp for media downloads..."
-pkg install ffmpeg -y
-pkg install python -y
-pip install yt-dlp
+# Install required packages
+echo "Installing Python, ffmpeg, and yt-dlp..."
+pkg install python ffmpeg -y
+pip install -U yt-dlp
 
 # Create download folders
 echo "Creating directories for downloads..."
-if [ ! -d "$STORAGE_PATH/Music/" ]; then
-    mkdir -p "$STORAGE_PATH/Music/"
-fi
-if [ ! -d "$STORAGE_PATH/YouTube/" ]; then
-    mkdir -p "$STORAGE_PATH/YouTube/"
-fi
-if [ ! -d "$STORAGE_PATH/Unknown/" ]; then
-    mkdir -p "$STORAGE_PATH/Unknown/"
-fi
+mkdir -p "$STORAGE_PATH/Music" "$STORAGE_PATH/YouTube" "$STORAGE_PATH/Unknown"
 
 # Set up enhanced URL opener script
-echo "Setting up the enhanced Termux URL Opener script..."
-if [ ! -d "$HOME/bin" ]; then
-    mkdir -p "$HOME/bin"
-fi
+echo "Setting up Termux URL Opener script..."
+mkdir -p "$HOME/bin"
 curl -L -o "$HOME/bin/termux-url-opener" "https://raw.githubusercontent.com/Rims-Naps/Termux-YT-DLG/main/termux-url-opener"
 chmod +x "$HOME/bin/termux-url-opener"
 
 # Set up yt-dlp config
 echo "Setting up yt-dlp configuration..."
-if [ ! -d "$HOME/.config/yt-dlp" ]; then
-    mkdir -p "$HOME/.config/yt-dlp"
-fi
-echo "-S ext:mp4:m4a,res:720 -o $STORAGE_PATH/YouTube/%(title)s.%(ext)s --no-mtime" > "$HOME/.config/yt-dlp/config"
-echo "-S ext:mp3 -o '$STORAGE_PATH/Music/%(title)s.%(ext)s' --no-mtime" >> "$HOME/.config/yt-dlp/config"
-echo "-S ext:mp4:m4a,res:720 -o $STORAGE_PATH/Unknown/%(title)s.%(ext)s --no-mtime" >> "$HOME/.config/yt-dlp/config"
+mkdir -p "$HOME/.config/yt-dlp"
+cat > "$HOME/.config/yt-dlp/config" <<EOL
+# yt-dlp configuration
+-S ext:mp4:m4a,res:720 -o "$STORAGE_PATH/YouTube/%(title)s.%(ext)s" --no-mtime --quiet --progress --force-overwrites
+-S ext:mp3 -o "$STORAGE_PATH/Music/%(title)s.%(ext)s" --no-mtime --quiet --progress --force-overwrites
+-S ext:mp4:m4a,res:720 -o "$STORAGE_PATH/Unknown/%(title)s.%(ext)s" --no-mtime --quiet --progress --force-overwrites
+EOL
 
 # Installation complete
 echo "Installation complete! Share a video or music link with Termux to start downloading."
